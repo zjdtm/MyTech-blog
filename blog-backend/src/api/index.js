a7d3f38691eb88ad199239f172ddea3f3d7f494d
zjdtm <syh05247@naver.com>
2022-10-13 20:57:31 +0900

    post mongoose crud

---
+++ blog-backend/src/api/index.js
@@ -0,0 +1,7 @@
+import Router from 'koa-router';
+import posts from './posts/index.js';
+const api = new Router();
+
+api.use('/posts', posts.routes());
+
+export default api;
---
+++ blog-backend/src/models/post.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const PostSchema = new Schema(
+  {
+    title: String,
+    body: String,
+    tags: [String],
+  },
+  { timestamps: true },
+);
+
+const Post = mongoose.model('Post', PostSchema);
+export default Post;
