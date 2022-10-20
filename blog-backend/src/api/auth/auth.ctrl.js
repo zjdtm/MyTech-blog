919764ee47620c9648f2198a75bae9255293472b
zjdtm <syh05247@naver.com>
2022-10-20 21:36:45 +0900

    login 구현

---
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -0,0 +1,43 @@
+import Joi from 'joi';
+import User from '../../models/user.js';
+
+export const register = async (ctx) => {
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string()
+      .pattern(new RegExp('/^(?=.*d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/'))
+      .required(),
+  });
+
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { username, password } = ctx.request.body;
+  try {
+    const exists = await User.findByUsername(username);
+    if (exists) {
+      ctx.status = 409;
+      return;
+    }
+    const user = new User({
+      username,
+    });
+
+    await user.setPassword(password);
+    await user.save();
+
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const login = async (ctx) => {};
+
+export const check = async (ctx) => {};
+
+export const logout = async (ctx) => {};
--- blog-backend/src/api/index.js
+++ blog-backend/src/api/index.js
@@ -1,7 +1,9 @@
 import Router from 'koa-router';
 import posts from './posts/index.js';
+import auth from './auth/index.js';
 const api = new Router();
 
 api.use('/posts', posts.routes());
+api.use('/auth', auth.routes());
 
 export default api;
